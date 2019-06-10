30d354ba643e27280f866a7760f0c9b95f6ec835
Daily120 <forzhacker@mail.ru>
2019-06-10 23:17:24 +0200

    piece of shit. needs refactoring

--- server.js
+++ server.js
@@ -38,6 +38,86 @@ app.get('/', (req, res) => {
     res.send("Hi from Express!")
 })
 
+app.post('/auth', (req, res) => {
+    db.select('username').from('auth').where("mac_address", "=", req.body.mac_address)
+    .then(data => {
+        if(data[0]) {
+            res.json(data[0].username);
+        } else {
+            res.status(400).json('new_user')
+        }
+    })
+    .catch(err => res.status(400).json('unable to get user'))
+})
+
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    if (req.body.mac_address.length == 17) {
+        db.insert({
+            mac_address: req.body.mac_address,
+            username: req.body.username
+        }).into('auth').returning('username')
+        .then(username => res.json(username))
+        .catch(err => res.status(400).json('user already exists'));
+    } else {
+        res.status(400).json('wrong mac address');
+    }
+})
+
+app.post('/add-question', (req, res) => {
+    console.log(req.body);
+    if (req.body.title) {
+        db.insert({
+            user_id: req.body.user_id,
+            title: req.body.title,
+            question_text: req.body.question_text
+        }).into('questions').returning('*')
+        .then(data => res.json(data))
+        .catch(err => res.status(400).json('unable to add question'))
+    } else {
+        res.status(400).json('bad request')
+    }
+})
+
+app.get('/questions', (req, res) => {
+    db.select('*').from('questions').join('auth', 'questions.user_id', 'auth.id').then(data => {
+        data.forEach(question => {
+            delete question.id
+            delete question.mac_address;
+            delete question.points;
+        })
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to get questions'))
+})
+
+app.get('/questions/:id/answers', (req, res) => {
+    const question_id = req.params.id;
+    db.select('*').from('answers').where('question_id', '=', question_id)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json(err))
+})
+
+app.post('/answer', (req, res) => {
+    const { question_id, user_id, answer_text } = req.body;
+    db.insert({
+        question_id,
+        user_id,
+        answer_text
+    }).into('answers').returning('id')
+    .then(id => res.json(id))
+    .catch(err => res.status(400).json('unable to add answer'))
+})
+
+app.post('/tip', async (req, res) => {
+    const { tip_source, tip_destination, answer_id } = req.body;
+    await db('auth').where('id', '=', tip_source).decrement('points', 5);
+    await db('auth').where('id', '=', tip_destination).increment('points', 5);
+    db('answers').where('id', '=', answer_id).increment('tips_count', 1)
+    .returning('tips_count').then(tips_count => res.json(tips_count))
+    .catch(err => res.status(400).json(err));
+})
+
 app.listen(3001, () => {
     console.log(`app is running on port 3001`);
 })
