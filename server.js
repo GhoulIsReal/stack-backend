5ad50ffb7ffb444e9740c287f7a51da05aac8142
Yerassyl <erasyl.boy@gmail.com>
2019-06-19 19:20:29 +0600

    first blood

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ app.get('/', (req, res) => {
 })
 
 app.post('/auth', (req, res) => {
-    db.select('username').from('auth').where("mac_address", "=", req.body.mac_address)
+    db.select('username', 'points', 'id').from('auth').where("mac_address", "=", req.body.mac_address.toLowerCase())
     .then(data => {
         if(data[0]) {
-            res.json(data[0].username);
+            res.json(data[0]);
         } else {
             res.status(400).json('new_user')
         }
@@ -54,8 +54,9 @@ app.post('/register', (req, res) => {
     console.log(req.body);
     if (req.body.mac_address.length == 17) {
         db.insert({
-            mac_address: req.body.mac_address,
-            username: req.body.username
+            mac_address: req.body.mac_address.toLowerCase(),
+            username: req.body.username,
+            points: 100
         }).into('auth').returning('username')
         .then(username => res.json(username))
         .catch(err => res.status(400).json('user already exists'));
@@ -66,13 +67,16 @@ app.post('/register', (req, res) => {
 
 app.post('/add-question', (req, res) => {
     console.log(req.body);
-    if (req.body.title) {
+    if (req.body.title && req.body.question_text) {
         db.insert({
             user_id: req.body.user_id,
             title: req.body.title,
             question_text: req.body.question_text
         }).into('questions').returning('*')
-        .then(data => res.json(data))
+        .then(async data => {
+            await db('auth').where('id', '=', req.body.user_id).decrement('points', 10);
+            res.json(data)
+        })
         .catch(err => res.status(400).json('unable to add question'))
     } else {
         res.status(400).json('bad request')
@@ -80,9 +84,10 @@ app.post('/add-question', (req, res) => {
 })
 
 app.get('/questions', (req, res) => {
-    db.select('*').from('questions').join('auth', 'questions.user_id', 'auth.id').then(data => {
+    db.select('*').from('questions').join('auth', 'questions.user_id', 'auth.id').orderBy('question_primary_id', 'desc').then(data => {
         data.forEach(question => {
-            delete question.id
+            const timestamp = new Date(Date.parse(question.published_date))
+            question.published_date = timestamp.toLocaleString('en-us', { month: 'short', day: 'numeric' });
             delete question.mac_address;
             delete question.points;
         })
@@ -93,8 +98,10 @@ app.get('/questions', (req, res) => {
 
 app.get('/questions/:id/answers', (req, res) => {
     const question_id = req.params.id;
-    db.select('*').from('answers').where('question_id', '=', question_id)
-    .then(data => res.json(data))
+    db.select('answers.id', 'answers.question_id', 'answers.user_id', 'answers.answer_text', 'auth.username', 'answers.tips_count').from('answers').where('question_id', '=', question_id).join('auth', 'answers.user_id', 'auth.id').orderBy('answers.id', 'asc')
+    .then(data => {
+        res.json(data)
+    })
     .catch(err => res.status(400).json(err))
 })
 
@@ -104,18 +111,27 @@ app.post('/answer', (req, res) => {
         question_id,
         user_id,
         answer_text
-    }).into('answers').returning('id')
-    .then(id => res.json(id))
+    }).into('answers').returning('*')
+    .then(async answerList => {
+        const username = await db.select('username').from('auth').where('id', '=', answerList[0].user_id)
+        answerList[0].username = username[0].username
+        console.log(answerList[0]);
+        res.json(answerList[0])
+    })
     .catch(err => res.status(400).json('unable to add answer'))
 })
 
 app.post('/tip', async (req, res) => {
     const { tip_source, tip_destination, answer_id } = req.body;
-    await db('auth').where('id', '=', tip_source).decrement('points', 5);
-    await db('auth').where('id', '=', tip_destination).increment('points', 5);
-    db('answers').where('id', '=', answer_id).increment('tips_count', 1)
-    .returning('tips_count').then(tips_count => res.json(tips_count))
-    .catch(err => res.status(400).json(err));
+    if(tip_source !== tip_destination) {
+        await db('auth').where('id', '=', tip_source).decrement('points', 5);
+        await db('auth').where('id', '=', tip_destination).increment('points', 5);
+        db('answers').where('id', '=', answer_id).increment('tips_count', 1)
+        .returning('tips_count').then(tips_count => res.json(tips_count))
+        .catch(err => res.status(400).json(err));
+    } else {
+        res.status(400).json("Bad request")
+    }
 })
 
 app.listen(3001, () => {
